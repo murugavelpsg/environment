4bb066db9ff2368d136676075f4e50fe7ec25e93
Murugavel Sivagnanam <msivagna@cisco.com>
2016-02-18 15:55:08 -0800

    Fixed the restarticfd script

--- scripts/restarticfd.sh
+++ scripts/restarticfd.sh
@@ -1,12 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
 cp /tmp/icfb-workflow-engine.jar /opt/icfb/workflow-engine/
 cp /tmp/icfb.war /opt/tomcat/webapps/icfb.war
 cp /tmp/icfb-cloud-providers.jar /opt/tomcat/icfb-libs/endorsed/delegates
-cp /tmp/icfb-messaging-server.jar /opt/icfb/messaging-server/
 ps -ef | grep messaging | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | grep workflow | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | grep tomcat | grep -v grep | awk '{print $2}' | xargs kill -9
 sleep 2
 cd /opt/icfb/workflow-engine/ && java  -Xms4096m -Xmx4096m -XX:MaxPermSize=512m -Xdebug -Xrunjdwp:transport=dt_socket,address=9042,server=y,suspend=n -cp icfb-workflow-engine.jar -jar icfb-workflow-engine.jar -httpPort 9000 &
-cd /opt/icfb/messaging-server; java -jar icfb-messaging-server.jar &
 cd /opt/tomcat/bin; ./catalina.sh jpda start
