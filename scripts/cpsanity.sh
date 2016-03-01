a81d5904bb033efec2143fc56edd0d1d093c9ad0
Murugavel Sivagnanam <msivagna@cisco.com>
2016-02-29 17:22:24 -0800

    Updating copy sanity

--- scripts/cpsanity.sh
+++ scripts/cpsanity.sh
@@ -3,5 +3,5 @@ cd ~/Projects/icfd-rest/
 scp icfb-thirdparty-libs/target/icfb-libs/endorsed/delegates/icfb-cloud-providers.jar msivagna@171.71.188.55:/ws/msivagna-sjc/sanity/
 scp icfb-workflow-engine/target/icfb-workflow-engine.jar msivagna@171.71.188.55:/ws/msivagna-sjc/sanity/
 scp icfb-thirdparty-libs/target/icfb-thirdparty.tar.gz msivagna@171.71.188.55:/ws/msivagna-sjc/sanity/
-scp icfb-messaging-server/target/icfb-messaging-server-0.9.2-SNAPSHOT.jar msivagna@171.71.188.55:/ws/msivagna-sjc/sanity/icfb-messaging-server.jar
+scp icfb-encryption-services/target/icfb-encryption-services-0.9.2-SNAPSHOT.jar msivagna@171.71.188.55:/ws/msivagna-sjc/sanity/icfb-encryption-services.jar
 scp icfb-skinny-war/target/icfb-skinny.war msivagna@171.71.188.55:/ws/msivagna-sjc/sanity/icfb.war
